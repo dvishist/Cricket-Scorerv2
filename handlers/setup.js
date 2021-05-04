e3959701d9a09deb4ec224cf4a559e35e4a34dee
dvishist <54056185+dvishist@users.noreply.github.com>
2021-05-05 09:59:04 +1000

    create match instance from setup window

--- handlers/setup.js
+++ handlers/setup.js
@@ -1,5 +1,6 @@
 const { ipcRenderer } = require('electron')
 const loadTeams = require('../match/teamsLoader')
+const createMatch = require('../match/createMatch')
 
 const form = document.querySelector('form')
 
@@ -7,13 +8,17 @@ form.addEventListener('submit', e => {
     e.preventDefault()
     const team1FileName = document.querySelector('#team1FileName').value
     const team2FileName = document.querySelector('#team2FileName').value
-    const overs = document.querySelector('#overs').value
-    const tossWinner = document.querySelector('#tossWinner').value
-    const decision = document.querySelector('#bat').checked
+    const oversText = document.querySelector('#overs').value
+    const tossWinnerName = document.querySelector('#tossWinner').value
+    const bat = document.querySelector('#bat').checked
 
     try {
         const { team1Obj, team2Obj } = loadTeams(team1FileName, team2FileName)
-
+        let tossWinner = tossWinnerName === team1FileName ? team1Obj : team2Obj
+        let decision = bat ? 'bat' : 'bowl'
+        let overs = parseInt(oversText)
+        const match = createMatch(team1Obj, team2Obj, tossWinner, decision, overs)
+        console.log(match)
     } catch (err) {
         alert('Please make sure file names are correct')
     }
