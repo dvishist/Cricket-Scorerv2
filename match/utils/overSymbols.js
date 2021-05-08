8f447625203675c88a49d0480117951940d3fce7
dvishist <54056185+dvishist@users.noreply.github.com>
2021-05-09 08:32:13 +1000

    update bowler display

---
+++ match/utils/overSymbols.js
@@ -0,0 +1,16 @@
+
+const overSymbols = {
+    dot: "⓿",
+    wicket: "Ⓧ",
+    noBall: "Ⓝ",
+    legBy: "Ⓛ",
+    by: "Ⓑ",
+    one: "①",
+    two: "②",
+    three: "③",
+    four: "④",
+    five: "⑤",
+    six: "⑥",
+}
+
+module.exports = overSymbols
